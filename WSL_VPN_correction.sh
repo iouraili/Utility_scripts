baceab399fb01bca53faed7a203905ad83ba410d
iouraili <48048431+iouraili@users.noreply.github.com>
2020-09-30 13:37:18 +0300

    Update WSL_VPN_correction.sh

--- WSL_VPN_correction.sh
+++ WSL_VPN_correction.sh
@@ -1,2 +1,4 @@
-# run this command as superuser (sudo -s)
+#!/usr/bin/env bash
+
+# run this script as superuser (sudo -s)
 /mnt/c/Windows/system32/ipconfig.exe /all | grep --color=auto "DNS Servers" | cut -d ":" -f 2 | grep --color=auto -e '^ [0-9]' | sed 's/^/nameserver/' |tr -d '\r' | sudo tee /etc/resolv.conf > /dev/null
